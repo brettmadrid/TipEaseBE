0177d22d572378dd8c4bffbabb4cb258c86bdbb0
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-13 23:06:34 -0600

    added .env file and refactored some code to make it dynamic depending on .env variables

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,5 @@
-const knex = require('knex');
+const dbEnvironment = process.env.DB_ENVIRONMENT || 'development';
 
-const dbConfig = require('../knexfile');
+const dbConfig = require('../knexfile')[dbEnvironment];
 
-module.exports = knex(dbConfig.development);
+module.exports = require('knex')(dbConfig);
