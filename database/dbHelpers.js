505140b2f924e06fdaf61c96411ddf1f9417161c
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-13 22:59:23 -0600

    cleaned up dbHelpers.js to get rid of some old copy/pasted code

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -2,20 +2,8 @@ const express = require('express');
 const db = require('./dbConfig');
 
 module.exports = {
-  insertUser,
-  findByUsername,
   getWorkers
 };
-
-function insertUser(user) {
-  return db('users').insert(user);
-}
-
-function findByUsername(username) {
-  return db('users')
-    .where('username', username)
-    .first();
-}
 
 function getWorkers() {
   return db('workers').select(
