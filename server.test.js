d99a38216eec833b9538cba0572cf998e546c4f4
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2019-02-18 22:19:42 -0600

    set up a test script on package.json and wrote a couple of tests. No test database has been set up yet.

--- server.test.js
+++ server.test.js
@@ -1,6 +1,33 @@
 const request = require('supertest');
 
 const server = require('./server');
-const db = require('./database/dbConfig');
+const db = require('./database/???');
 
 // Test file set up and ready for tests to be written
+describe('the route handlers', () => {
+  describe('Register functionality', () => {
+    test('responds with a 201 status when a customer registers', async () => {
+      const body = {
+        username: 'jcand12',
+        password: 'pass',
+        accountType: 'customer'
+      };
+      const response = await request(server)
+        .post('/register')
+        .send(body);
+      expect(response.status).toBe(201);
+    });
+    test('responds with a 201 status when a worker registers', async () => {
+      const body = {
+        username: 'jcand12',
+        password: 'pass',
+        accountType: 'worker'
+      };
+      const response = await request(server)
+        .post('/register')
+        .send(body);
+      expect(response.status).toBe(201);
+    });
+  });
+  describe('Login functionality', () => {});
+});
